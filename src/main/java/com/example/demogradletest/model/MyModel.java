5b69891878860ee23b0be01dfab518e64637b209
Your Name <you@example.com>
2021-03-29 19:26:58 +0700

    renamed site to /somethingChanged

--- src/main/java/com/example/demogradletest/model/MyModel.java
+++ src/main/java/com/example/demogradletest/model/MyModel.java
@@ -1,17 +1,12 @@
 package com.example.demogradletest.model;
+import lombok.Data;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@Data
 public class MyModel {
     private String name;
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
 
     @Override
     public String toString() {
